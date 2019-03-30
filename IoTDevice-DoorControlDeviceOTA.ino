b73b6ea79f1f57f21beaa01b948f9f58761543ba
Osama Oransa <osa_ora@yahoo.com>
2019-03-30 20:56:53 +0300

    Add sensor2AsOutput variable

--- IoTDevice-DoorControlDeviceOTA.ino
+++ IoTDevice-DoorControlDeviceOTA.ino
@@ -35,6 +35,7 @@
 #define BUZZER_PIN D7
 //Connected Sensors Count 
 int connectedSensors = 1;
+boolean sensor2AsOutput=false;
 //Connected Relay Modules Count 
 int connectedDevices = 1;
 //Is Buzzer attached for local alarm?
